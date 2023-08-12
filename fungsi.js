b0ecac2a983aac1d14655e8b0fc9d9d4729a2ad0
Muhammad Wahyu Prastyo <fearless12314@gmail.com>
2023-08-12 14:28:07 +0700

    merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,9 +1,13 @@
 console.log('fugsi pertama');
 const add = (a,b) => a+b;
+<<<<<<< HEAD
 console.log(add(1,2));
 
-const multiply = (a,b) => a*b;
+const multiplyNumber = (a,b) => a*b;
 console.log(multiply(2,3));
 
 const devided =(a,b) => a/b;
 console.log(devided(2,2));
+=======
+console.log(add(1,2));
+>>>>>>> parent of 33048af (menambahkan fitur perkalian)
